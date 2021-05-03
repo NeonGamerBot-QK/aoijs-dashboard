c6a667f54c1ea87c0cf86bf6c46ed7cbf5c8dbfb
Neon <58448036+NeonGamerBot-QK@users.noreply.github.com>
2021-05-03 10:28:26 -0400

    Create config.js
    the config file

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+monguri: 'urmongurihere',
+token: 'bot token',
+secret: 'client_secret',
+id: 'bots id',
+session_secret: 'sessionsecret',
+ownerID: ['ownerid', 'other ownerID'], // keep this an array 
+}
+module.exports = config;
