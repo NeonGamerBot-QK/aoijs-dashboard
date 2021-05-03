c5f7ff00eda1490d4c5190ec5f305faa13317714
Neon <58448036+NeonGamerBot-QK@users.noreply.github.com>
2021-05-03 13:17:13 -0400

    makingit json

--- config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-monguri: 'urmongurihere',
-token: 'bot token',
-secret: 'client_secret',
-id: 'bots id',
-session_secret: 'sessionsecret',
-ownerID: ['ownerid', 'other ownerID'], // keep this an array 
-}
-module.exports = config;
