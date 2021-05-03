cff178ac3d410e342e278f3c11e80d4895f92cfb
Neon <58448036+NeonGamerBot-QK@users.noreply.github.com>
2021-05-03 11:01:51 -0400

    Create add.js

---
+++ add.js
@@ -0,0 +1,18 @@
+const fetech = require(`node-fetch`);//p
+const config = require(`./config`)
+const TOKEN = config.token;
+const guild = 'the guild id'
+async function adduser(req) {
+    const fetech = require(`node-fetch`);
+const user = req.user
+    const response = await fetech(`https://discord.com/api/v8/guilds/${guild}/members/${user.id}`, {
+    method: 'PUT',
+    headers: {
+        Authorization: `Bot ${TOKEN}`, // authorisaing the bot to add them
+        "Content-type":'application/json'
+    },   
+    body: JSON.stringify({ access_token: user.accessToken })
+});
+return response.json();
+}
+module.exports = ( adduser() )
